076f1ae998ce45db2cfc4d7fefb15db0688e89b9
Bruce Clayton <bruce@measuredsearch.com>
2019-03-04 13:24:23 -0800

    Alert API scripts
    PowerShell scripts exercising the Alert API.

---
+++ restapi/accountalertsmetrics.ps1
@@ -0,0 +1,41 @@
+# Script for listing the alerts metrics of an account. 
+
+# Removes TLS obstacles from connection. Otherwise connections fail. 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+
+$USER = "bruce@searchstax.com"
+$PASSWORD = $( Read-Host "Input password, please" -AsSecureString) 
+$PASSWORD = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($PASSWORD))
+$ACCOUNT = "SilverQAAccount"
+
+Write-Host "Asking for an authorization token for $USER..."
+Write-Host
+
+$body = @{
+    username=$USER
+    password=$PASSWORD
+}
+Remove-Variable PASSWORD
+
+$body = $body | ConvertTo-Json
+
+$TOKEN = Invoke-RestMethod -uri "https://app.searchstax.com/api/rest/v2/obtain-auth-token/" -Method Post -Body $body -ContentType 'application/json' 
+$TOKEN = $TOKEN.token
+Remove-Variable body
+
+Write-Host "Obtained TOKEN" $TOKEN
+Write-Host
+
+# Set up HTTP header for authorization token
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", "Token $TOKEN")
+
+# GET /api/rest/v2/account/<account>/alerts/metrics/
+
+$RESULTS = Invoke-RestMethod -uri "https://app.searchstax.com/api/rest/v2/account/$ACCOUNT/alerts/metrics/" -Method Get -Headers $headers
+$RESULTS = $RESULTS | ConvertTo-Json
+
+Write-Host $RESULTS
+
+Write-Host "Exit..."
+Exit
