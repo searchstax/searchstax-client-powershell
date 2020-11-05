5bf5a638464c8ced496144452a908e095161a9df
Bruce Clayton <bruce@measuredsearch.com>
2020-11-05 11:11:12 -0800

    Updates

--- restapi/accountdeploymentipfilterdeletecidrip.ps1
+++ restapi/accountdeploymentipfilterdeletecidrip.ps1
@@ -17,39 +17,43 @@
 # account > deployment > ip-filter > delete-cidr-ip
 # PowerShell script for deleting an IP Filter from a deployment.
 
+# TOKEN authentication commented out to test APIkey authentication. 
+
 # Removes TLS obstacles from connection. Otherwise connections fail. 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
 
-$USER = "bruce@searchstax.com"
-$PASSWORD = $( Read-Host "Input password, please" -AsSecureString) 
-$PASSWORD = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($PASSWORD))
-$ACCOUNT = "SilverSupportAccount"
-$uid = "ss380502"
+#$USER = "user@company.com"
+#$PASSWORD = $( Read-Host "Input password, please" -AsSecureString) 
+#$PASSWORD = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($PASSWORD))
+$ACCOUNT = "AccountName"
+$uid = "ss123456"
+$APIKEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI2Njg1NWRkNDQxOGI2OGQ2M2U3MTdlMDMwMjk2NWE5NzY1ZmFlMWMwIiwiaWF0IjoxNTc0MDE5NjA0LCJ0ZW5hbnRfYXBpX2FjY2Vzc19rZXkiOiI1MVF5a3FGdlRvK0pjeVNqOXdNd1pRIiwic2NvcGUiOlsiZGVwbG95bWVudC5kZWRpY2F0ZWRkZXBsb3ltZW50Il19.LNZRn-O44C-hLHS62IGv2megf4Qw2z5I_WBsOjWeyVI"
 
 Write-Host "Asking for an authorization token for $USER..."
 Write-Host
 
-$body = @{
-    username=$USER
-    password=$PASSWORD
-}
-Remove-Variable PASSWORD
+#$body = @{
+#    username=$USER
+#    password=$PASSWORD
+#}
+#Remove-Variable PASSWORD
 
-$body = $body | ConvertTo-Json
+#$body = $body | ConvertTo-Json
 
-$TOKEN = Invoke-RestMethod -uri "https://app.searchstax.com/api/rest/v2/obtain-auth-token/" -Method Post -Body $body -ContentType 'application/json' 
-$TOKEN = $TOKEN.token
-Remove-Variable body
+#$TOKEN = Invoke-RestMethod -uri "https://app.searchstax.com/api/rest/v2/obtain-auth-token/" -Method Post -Body $body -ContentType 'application/json' 
+#$TOKEN = $TOKEN.token
+#Remove-Variable body
 
 Write-Host "Obtained TOKEN" $TOKEN
 Write-Host
 
 # Set up HTTP header for authorization token
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", "Token $TOKEN")
+#$headers.Add("Authorization", "Token $TOKEN")
+$headers.Add("Authorization", "APIkey $APIKEY")
 
 $body = @{
-    cidr_ip='100.100.100.100'
+    cidr_ip='0.0.0.0/0'
 }
 
 $body = $body | ConvertTo-Json
