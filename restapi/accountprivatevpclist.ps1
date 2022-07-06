ffcc81c6935ae8075434f9aed4f9a4605d66ec04
Bruce Clayton <bruce@measuredsearch.com>
2022-07-06 06:49:06 -0700

    Create accountprivatevpclist.ps1

---
+++ restapi/accountprivatevpclist.ps1
@@ -0,0 +1,66 @@
+#   Copyright 2022 SearchStax, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#   -----------------------------------------------------------------------
+   
+# account > privatevpc > list
+# PowerShell script for listing the VPCs of an account.
+
+# Removes TLS obstacles from connection. Otherwise connections fail. 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+
+$USER = "user@company.com"
+$PASSWORD = $( Read-Host "Input password, please" -AsSecureString) 
+$PASSWORD = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($PASSWORD))
+$ACCOUNT = "AccountName"
+
+Write-Host "Asking for an authorization token for $USER..."
+Write-Host
+
+$body = @{
+    username=$USER
+    password=$PASSWORD
+}
+Remove-Variable PASSWORD
+
+$body = $body | ConvertTo-Json
+
+$TOKEN = Invoke-RestMethod -uri "https://app.searchstax.com/api/rest/v2/obtain-auth-token/" -Method Post -Body $body -ContentType 'application/json' 
+Remove-Variable body
+
+Write-Host "Obtained TOKEN" $TOKEN.token
+$TOKEN = $TOKEN.token
+Write-Host $TOKEN
+Write-Host
+
+# Set up HTTP header for authorization token
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", "Token $TOKEN")
+
+#GET https://app.searchstax.com/api/rest/v2/account/<account_name>/privatevpc/?page=1
+
+Write-Host "Getting list of VPCs associated with" $ACCOUNT
+
+$RESULT = Invoke-RestMethod -Method Get -Headers $headers `
+         -uri "https://app.searchstax.com/api/rest/v2/account/$ACCOUNT/privatevpc/?page=1" 
+$RESULT = $RESULT | ConvertTo-Json
+
+Write-Host $RESULT
+Write-Host
+
+Write Host "Exit..."
+Exit
+
+
+
